965b0f9b68764257edd029c46aed6a065634fb69
HonzaSvaton <jan.svaton@outlook.cz>
2025-04-01 22:56:00 +0200

    Add files via upload

---
+++ kalkulacka.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Napiš svoje první číslo: ");
+double prvnicislo = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Napiš svoje druhý číslo: ");
+double druhecislo = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Napiš jaké znamínko chceš použít: ");
+string znamenko = Console.ReadLine();
+if (znamenko != "+" && znamenko != "-" && znamenko != "/" && znamenko != "*")
+{
+    Console.WriteLine("Zadal jsi neplatné znaménko zadej znovu!!" + "Zadej prosím + - * /");
+}
+else
+{
+    if (druhecislo == 0 && znamenko == "/")
+    {
+        Console.WriteLine("Nulou nelzde dělit!!");
+    }
+    else
+    {
+        if (znamenko == "+")
+        {
+            Console.WriteLine($"Výsledek:  {prvnicislo + druhecislo}");
+        }
+        else if (znamenko == "-")
+        {
+            Console.WriteLine($"Výsledek: {prvnicislo - druhecislo}");
+        }
+        else if (znamenko == "*")
+        {
+            Console.WriteLine($"Výsledek: {prvnicislo * druhecislo}");
+        }
+        else if (znamenko == "/")
+        {
+            Console.WriteLine($"Výsledek: {prvnicislo / druhecislo}");
+        }
+    }
+}
